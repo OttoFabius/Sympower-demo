e4be63514caada752563292722b98cdbd9758cff
Otto <ottofabius@gmail.com>
2024-12-29 17:44:51 +0100

    small edit

--- run_streamlit.py
+++ run_streamlit.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from energy_functions import *  # Import your logic
-import plotly as pl
+import plotly.express as px
 
 
 #read dayahead prices
@@ -14,7 +14,7 @@ print(prices_df.shape)
 user_price = st.number_input("Enter a price to compare with the hourly prices (USD):", min_value=0.0, format="%.2f")
 
 # Create Plotly figure for visualization
-fig = pl.express.line(prices_onefulldayahead, x='utc_timestamp', y='Day-ahead Energy Price', title='Hourly Electricity Prices vs User Input Price', markers=True)
+fig = px.line(prices_onefulldayahead, x='utc_timestamp', y='Day-ahead Energy Price', title='Hourly Electricity Prices vs User Input Price', markers=True)
 
 # Highlight only the points where the price is lower than the user input price
 prices_to_show = prices_onefulldayahead[prices_onefulldayahead['Day-ahead Energy Price'] < user_price]
